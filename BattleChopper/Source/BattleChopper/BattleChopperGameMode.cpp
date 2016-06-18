b25f07da74081f9cd9a02f5f216a825b2fe2df51
Bryant McCracken <bryantdmccracken2005@gmail.com>
2016-06-18 00:06:16 -0500

    Created Blank Project

---
+++ BattleChopper/Source/BattleChopper/BattleChopperGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleChopper.h"
+#include "BattleChopperGameMode.h"
+
+
+
+
---
+++ BattleChopper/Source/BattleChopper/BattleChopperGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleChopperGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLECHOPPER_API ABattleChopperGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
